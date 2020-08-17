62549b81c63d478e92367898a84bae28d2896934
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-17 16:34:43 +0530

    Completed the front-end part
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const http = require('http').createServer(app)
 
 const PORT = process.env.PORT || 3000
 
+app.use(express.static(__dirname+'/public'))
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + 'index.html')
 })
 
 http.listen(PORT, () => {
