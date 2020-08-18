c0c2090f943937bb45a1fdb35d7ce175a0020470
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-18 22:52:32 +0530

    Sending and receiving message
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const io = require('socket.io')(http)
 
 io.on('connection', (socket) => {
     console.log('Connected...')
+
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message',msg)
+    })
 })
 
 http.listen(PORT, () => {
