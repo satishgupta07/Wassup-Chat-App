faa6920d4d93e1b5502870b7736e0965573389c1
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-18 22:10:51 +0530

    setup socket.io
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + 'index.html')
 })
 
+
+// Socket
+
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+})
+
 http.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`);
 })
